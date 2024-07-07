a73c180db3bbe60ba6b27eeaedea767855c35983
yashborda7048 <yashborda123@gmail.com>
2024-07-07 11:32:39 +0530

    first commit

---
+++ assets/js/myscript.js
@@ -0,0 +1,60 @@
+$('.fade-img').slick({
+  infinite: true,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 2000,
+  arrows:false,
+  fade: true,
+  cssEase: 'linear',
+  pauseOnHover:false,
+});
+
+$('.autoplay').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    centerMode: true,
+    centerPadding: '0px',
+    vertical: true,
+    verticalSwiping: false,
+    arrows:false,
+    pauseOnHover:false,
+  });
+
+  $('.text1-h1').slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    arrows:false,
+    fade: true,
+    cssEase: 'linear',
+    pauseOnHover:false,
+  });
+
+  $('.text2').slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    arrows:false,
+    fade: true,
+    cssEase: 'linear',
+    pauseOnHover:false,
+  });
+  $('.text3-h1').slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    arrows:false,
+    fade: true,
+    cssEase: 'linear',
+    pauseOnHover:false,
+  });
+  
